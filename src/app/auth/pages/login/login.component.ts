68195fbe967bb5a19aa95bed3ab346cbdf3e530a
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-20 09:25:38 -0600

    AuthService creation

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,19 @@ import { Router } from '@angular/router';
   styles: [],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   login() {
     // Ir al backend
     // Un usuario
-    this.router.navigate(['./heroes']);
+    this.authService.login().subscribe((resp) => {
+      console.log(resp);
+
+      if (resp.id) {
+        this.router.navigate(['./heroes']);
+      }
+    });
+
+    // this.router.navigate(['./heroes']);
   }
 }
