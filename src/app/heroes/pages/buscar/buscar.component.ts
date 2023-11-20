ada0e1da13ae89b7f07a9c521d2a259afb1ab6dd
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-20 07:11:20 -0600

    Autocomplete message

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -11,17 +11,23 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 export class BuscarComponent {
   termino: string = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
 
   constructor(private heroesService: HeroesService) {}
 
   buscando() {
     this.heroesService
-      .getSugerencias(this.termino)
+      .getSugerencias(this.termino.trim())
       .subscribe((heroes) => (this.heroes = heroes));
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    // Validar opcion string vacio
+    if (!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
 
