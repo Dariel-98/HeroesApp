e7aa1a54de79e03615f0489ceae5d46d0328d7e5
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-20 09:14:06 -0600

    Basic Login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  login() {
+    // Ir al backend
+    // Un usuario
+    this.router.navigate(['./heroes']);
+  }
 }
